f5c393106b88d212069974c4623df4275bcd69dd
ryzhovalex <thed4rkof@gmail.com>
2024-08-01 12:49:20 +0300

    update token.rs

--- src/token.rs
+++ src/token.rs
@@ -1,4 +1,4 @@
-use crate::res::Res;
+use crate::{res::Res, time::delta};
 use hmac::{Hmac, Mac};
 use jwt::SignWithKey;
 use serde::Serialize;
@@ -7,12 +7,25 @@ use sha2::Sha256;
 #[derive(Serialize)]
 struct RtClaims {
     user_id: u32,
+    exp: f64
 }
 
-// TODO: add exp
 pub fn create_rt(user_id: u32) -> Res<String> {
     let secret: Hmac<Sha256> = Hmac::new_from_slice(b"weloveauth").unwrap();
-    let user_sid = "tmp".to_string();
-    let mut claims = RtClaims { user_id: user_id };
+    let mut claims = RtClaims {
+        user_id: user_id,
+        // 1 year for now
+        exp: delta((360 * 24 * 60 * 60).into())
+    };
+    Ok(claims.sign_with_key(&secret).unwrap())
+}
+
+pub fn create_at(user_id: u32) -> Res<String> {
+    let secret: Hmac<Sha256> = Hmac::new_from_slice(b"helloworld").unwrap();
+    let mut claims = RtClaims {
+        user_id: user_id,
+        // 1 year for now, even for at
+        exp: delta((360 * 24 * 60 * 60).into())
+    };
     Ok(claims.sign_with_key(&secret).unwrap())
 }
