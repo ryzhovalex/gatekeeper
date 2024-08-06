79e4c4f685d30c68b8bab8c71831b25a7d0ed2a9
ryzhovalex <thed4rkof@gmail.com>
2024-08-06 20:42:33 +0300

    update corund.cfg.yml, Cargo.lock, Cargo.toml, README.md, ...

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Gatekeeper
+# corund
 
 Standalone auth server.
--- src/main.rs
+++ src/main.rs
@@ -252,7 +252,7 @@ fn verify_domain_secret_from_header(
 fn main() {
     colog::init();
 
-    let mut file = File::open(path::cwd().unwrap().join("gatekeeper.cfg.yml")).unwrap();
+    let mut file = File::open(path::cwd().unwrap().join("corund.cfg.yml")).unwrap();
     let mut content = String::new();
     file.read_to_string(&mut content).unwrap();
 
